891b49e155c0f22546f4e8124e118a39f729af20
Jeremy <adbrizz@gmail.com>
2025-04-17 16:57:17 +0500

    Create fn to sort photos either randomly or by comments count

--- js/filters.js
+++ js/filters.js
@@ -1,3 +1,4 @@
+const PICTURES_COUNT = 10;
 const Filter = [
   DEFAULT = 'filter-default',
   RANDOM = 'filter-random',
@@ -13,4 +14,20 @@ const turnFilterOn = (loadedPictures) => {
   filtersElement.classList.remove('img-filters--inactive');
   pictures = [...loadedPictures];
   currentFilter = Filter.DEFAULT;
+};
+
+const sortRandomly = () => Math.random() - 0.5;
+
+const sortByComments = (pictureA, pictureB) =>
+    pictureB.comments.length - pictureA.comments.length;
+
+const sortPictures = () => {
+  switch (currentFilter) {
+    case Filter.RANDOM:
+      return [...pictures].sort(sortRandomly).slice(0, PICTURES_COUNT);
+    case Filter.DISCUSSED:
+      return [...pictures].sort(sortByComments);
+    case Filter.DEFAULT:
+      return [...pictures];
+  }
 };
